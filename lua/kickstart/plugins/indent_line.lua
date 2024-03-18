d4761b9de4f1e0b6c4bc2e8acae201de7240bca3
44mira <legolaslada@gmail.com>
2024-03-19 00:22:22 +0800

    config

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,10 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        char = '┊',
+      },
+    },
   },
 }
