580bef6f7646f9358b92af4bf81997635791c9e3
44mira <legolaslada@gmail.com>
2024-03-30 06:44:41 +0800

    feat: add markdown preview, refactor: moved keymaps to init.lua

--- init.lua
+++ init.lua
@@ -4,92 +4,6 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   TYRAEL.VIM       ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-    
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know how the Neovim basics, you can skip this step)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not sure exactly what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or neovim features used in kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your nvim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -156,7 +70,37 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 8
+
+-- [[ tyraelvim Keymaps ]]
+
+-- Toggle the NvimTree
+vim.keymap.set('n', '<leader>f', '<cmd>:NvimTreeToggle<CR>', { desc = 'Open [F]ile Tree' })
+
+-- Toggle lines for indented
+vim.keymap.set('n', '<leader>i', '<cmd>:IBLToggle<CR>', { desc = 'Toggle [I]ndent Blank Line' })
+
+-- Additional normal bind shortcut for ergonomics
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Normal mode' })
+
+-- Close a tab
+vim.keymap.set('n', '<leader>bd', '<cmd>bd<CR>', { desc = '[B]uffer [D]elete' })
+
+-- Cycle through tabs
+vim.keymap.set('n', '[b', '<cmd>:BufferLineCyclePrev<CR>', { desc = 'Previous [B]uffer' })
+vim.keymap.set('n', ']b', '<cmd>:BufferLineCycleNext<CR>', { desc = 'Next [B]uffer' })
+
+-- Rearrange tabs
+vim.keymap.set('n', '<leader>}', '<cmd>:BufferLineMoveNext<CR>', { desc = '[}] Forward Buffer' })
+vim.keymap.set('n', '<leader>{', '<cmd>:BufferLineMovePrev<CR>', { desc = '[{] Backward Buffer' })
+
+-- Jump to tab
+for i = 1, 9 do
+  vim.keymap.set('n', ('<M-%d>'):format(i), ('<cmd>:BufferLineGoToBuffer %d<CR>'):format(i), { desc = ('Go to Buffer [%d]'):format(i) })
+end
+
+-- Optional Racket Lambda bind
+-- vim.keymap.set('n', '<leader>l', 'iλ<Esc>', { desc = 'Insert Lambda' })
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -164,9 +108,6 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-vim.keymap.set('n', '<leader>f', '<cmd>:NvimTreeToggle<CR>', { desc = 'Open [F]ile Tree' })
-vim.keymap.set('n', '<leader>i', '<cmd>:IBLToggle<CR>', { desc = 'Toggle [I]ndent Blank Line' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -182,12 +123,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
@@ -196,15 +131,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-vim.keymap.set('n', '<leader>bd', '<cmd>bd<CR>', { desc = '[B]uffer [D]elete' })
-vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Normal mode' })
-vim.keymap.set('n', '<leader>l', 'iλ<Esc>', { desc = 'Insert Lambda' })
-
--- vim.keymap.set('n', '<C-h>', '<cmd><C-U>TmuxNavigateLeft<CR>', { desc = 'Tmux: Window Left' })
--- vim.keymap.set('n', '<C-l>', '<cmd><C-U>TmuxNavigateRight<CR>', { desc = 'Tmux: Window Right' })
--- vim.keymap.set('n', '<C-j>', '<cmd><C-U>TmuxNavigateDown<CR>', { desc = 'Tmux: Window Down' })
--- vim.keymap.set('n', '<C-k>', '<cmd><C-U>TmuxNavigateUp<CR>', { desc = 'Tmux: Window Up' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -244,15 +170,6 @@ require('lazy').setup {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 
   -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
 
   { 'numToStr/Comment.nvim', opts = {} },
   -- Here is a more advanced example where we pass configuration
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,16 +1,6 @@
 local textblocks = dofile 'C:\\Users\\Legolas\\.config\\nvim\\textblocks.lua'
 
 return {
-  {
-    'christoomey/vim-tmux-navigator',
-    keys = {
-      { '<M-\\>', '<cmd>TmuxNavigatePrevious<cr>', desc = 'Go to the previous pane' },
-      { '<M-h>', '<cmd>TmuxNavigateLeft<cr>', desc = 'Go to the left pane' },
-      { '<M-j>', '<cmd>TmuxNavigateDown<cr>', desc = 'Go to the down pane' },
-      { '<M-k>', '<cmd>TmuxNavigateUp<cr>', desc = 'Go to the up pane' },
-      { '<M-l>', '<cmd>TmuxNavigateRight<cr>', desc = 'Go to the right pane' },
-    },
-  },
   { 'm4xshen/autoclose.nvim', opts = {} },
   { 'windwp/nvim-ts-autotag', opts = {} },
   {
@@ -20,59 +10,49 @@ return {
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
-    config = function()
-      require('nvim-tree').setup {
-        view = {
-          side = 'right',
-        },
-      }
-    end,
+    opts = {
+      view = {
+        side = 'right',
+      },
+    },
   },
   {
     'MeanderingProgrammer/dashboard.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('dashboard').setup {
-        header = textblocks.header(),
-        directories = {
-          [[~\.config\nvim]],
-          [[~\Desktop\pandora\ACADEMICS\prac]],
-          [[C:\Users\Legolas\AppData\Roaming\alacritty]],
-        },
-        footer = { textblocks.footers() },
-        highlight_groups = {
-          header = 'bufferLineCurrent',
-          icon = 'bufferLineCurrent',
-          directory = 'jsParen',
-          hotkey = 'AerialOperatorIcon',
-          footer = 'jsParen',
-        },
-      }
-    end,
+    opts = {
+      header = textblocks.header(),
+      directories = {
+        [[~\.config\nvim]],
+        [[~\Desktop\pandora\ACADEMICS\prac]],
+        [[C:\Users\Legolas\AppData\Roaming\alacritty]],
+      },
+      footer = { textblocks.footers() },
+      highlight_groups = {
+        header = 'bufferLineCurrent',
+        icon = 'bufferLineCurrent',
+        directory = 'jsParen',
+        hotkey = 'AerialOperatorIcon',
+        footer = 'jsParen',
+      },
+    },
   },
   {
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      vim.opt.termguicolors = true
-      require('bufferline').setup {
-        options = {
-          separator_style = 'slope',
-        },
-      }
-
-      local map = vim.keymap.set
-
-      map('n', '[b', '<cmd>:BufferLineCyclePrev<CR>', { desc = 'Previous [B]uffer' })
-      map('n', ']b', '<cmd>:BufferLineCycleNext<CR>', { desc = 'Next [B]uffer' })
-      map('n', '<leader>}', '<cmd>:BufferLineMoveNext<CR>', { desc = '[}] Forward Buffer' })
-      map('n', '<leader>{', '<cmd>:BufferLineMovePrev<CR>', { desc = '[{] Backward Buffer' })
-
-      for i = 1, 9 do
-        map('n', ('<M-%d>'):format(i), ('<cmd>:BufferLineGoToBuffer %d<CR>'):format(i), { desc = ('Go to Buffer [%d]'):format(i) })
-      end
+    opts = {
+      options = {
+        separator_style = 'slope',
+      },
+    },
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
     end,
   },
 }
