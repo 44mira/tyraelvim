1d72ddb93b44c339af77f5d9412d7c6050710f7d
44mira <legolaslada@gmail.com>
2024-05-26 16:20:43 +0800

    feat: add copilot icon to lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,10 +1,24 @@
 local function lualine_name()
-  return [[sharmaigne 󰋑 ]]
+  return [[sharmaigne  ]]
+end
+
+local function copilot()
+  if (vim.b.copilot_enabled or 1) == 1 then
+    return ' '
+  end
+  return ' '
+end
+
+local function copilot_color()
+  if (vim.b.copilot_enabled or 1) == 1 then
+    return { fg = '#993a55' }
+  end
+  return { fg = '#82cfb6' }
 end
 
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'github/copilot.vim' },
   opts = {
     options = {
       section_separators = { left = '', right = '' },
@@ -12,7 +26,8 @@ return {
     },
     sections = {
       lualine_b = { 'branch' },
-      lualine_x = { 'diff', 'diagnostics', 'filetype' },
+      lualine_x = { 'diff', { copilot, color = copilot_color }, 'diagnostics' },
+      lualine_y = { 'filetype' },
       lualine_z = { lualine_name },
     },
     extensions = {
