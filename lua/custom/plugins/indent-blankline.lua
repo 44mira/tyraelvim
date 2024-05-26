83145999fcaad7ac84144a66fc7598e4f843fa0c
44mira <legolaslada@gmail.com>
2024-05-26 16:21:06 +0800

    feat: re-add ibl

---
+++ lua/custom/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {
+    scope = {
+      enabled = true,
+      show_start = false,
+    },
+    enabled = false,
+  },
+}
