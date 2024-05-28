44835f15e1699ba38af796276bae29a57d97d769
44mira <legolaslada@gmail.com>
2024-05-29 05:14:54 +0800

    feat: add copilot.lua

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,13 +1,45 @@
 return {
-  'github/copilot.vim',
-  lazy = false,
+  'zbirenbaum/copilot.lua',
+  lazy = true,
+  cmd = 'Copilot',
+  event = 'InsertEnter',
   config = function()
-    vim.b.copilot_enabled = 'v:false'
+    local copilot = require 'copilot'
 
-    vim.keymap.set('n', '<leader>gh', function()
-      vim.b.copilot_enabled = not vim.b.copilot_enabled
+    copilot.setup {
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = '[[',
+          jump_next = ']]',
+          accept = '<CR>',
+          refresh = 'gr',
+          open = '<M-CR>',
+        },
+        layout = {
+          position = 'right', -- | top | left | right
+          ratio = 0.4,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        debounce = 75,
+        keymap = {
+          accept = '<M-l>',
+          accept_word = '<M-Right>',
+          accept_line = '<M-Down>',
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
+      },
+    }
 
-      vim.cmd 'Copilot status'
-    end, { desc = 'Toggle [G]it[h]ub Copilot' })
+    vim.keymap.set('n', '<leader>cp', '<cmd>Copilot panel<CR>', { desc = 'Open [C]opilot [P]anel' })
+    vim.keymap.set('n', '<leader>ct', function()
+      require('copilot.suggestion').toggle_auto_trigger()
+    end, { desc = 'Toggle [C]opilot Auto-[t]rigger' })
   end,
 }
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,23 +2,33 @@ local function lualine_name()
   return [[sharmaigne  ]]
 end
 
-local function copilot()
-  if (vim.b.copilot_enabled or 1) == 1 then
-    return ' '
-  end
-  return ' '
-end
 
-local function copilot_color()
-  if (vim.b.copilot_enabled or 1) == 1 then
-    return { fg = '#993a55' }
-  end
-  return { fg = '#82cfb6' }
-end
+local copilot = {
+  'copilot',
+  symbols = {
+    status = {
+      icons = {
+        enabled = ' ',
+        sleep = ' ', -- auto-trigger disabled
+        disabled = ' ',
+        warning = ' ',
+        unknown = ' ',
+      },
+      hl = {
+        enabled = '#82CFB6',
+        sleep = '#6272A4',
+        disabled = '#6272A4',
+        warning = '#FFB86C',
+        unknown = '#FF5555',
+      },
+    },
+  },
+  show_colors = true,
+}
 
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons', 'github/copilot.vim' },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'AndreM222/copilot-lualine' },
   opts = {
     options = {
       section_separators = { left = '', right = '' },
@@ -27,6 +37,7 @@ return {
     sections = {
       lualine_b = { 'branch' },
       lualine_x = { 'diff', { copilot, color = copilot_color }, 'diagnostics' },
+      lualine_x = { 'diff', copilot, 'diagnostics' },
       lualine_y = { 'filetype' },
       lualine_z = { lualine_name },
     },
