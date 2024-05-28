c2b34c82e993a53d8c4f66c5c4336d625746297d
44mira <legolaslada@gmail.com>
2024-05-29 05:15:41 +0800

    feat: add default theme configurations

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,19 +1,67 @@
 -- You can easily change to a different colorscheme.
 
+local catppuccin = {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup {
+      integrations = {
+        treesitter = true,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+        indent_blankline = {
+          enabled = true,
+          scope_color = 'lavender', -- catppuccin color (eg. `lavender`) Default: text
+          colored_indent_levels = false,
+        },
+        cmp = true,
+        gitsigns = true,
+        harpoon = true,
+        aerial = true,
+        mason = true,
+        neotree = true,
+        which_key = true,
+      },
+    }
+
+    vim.cmd.colorscheme 'catppuccin'
+    vim.opt.fillchars = { eob = ' ' }
+  end,
+}
+
+local rosepine = {
+  'rose-pine/neovim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'rosepine'
+    vim.opt.fillchars = { eob = ' ' }
+  end,
+}
+
+local gruvbox_material = {
+  'sainnhe/gruvbox-material',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_enable_bold = true
+    vim.cmd.colorscheme 'gruvbox-material'
+    vim.opt.fillchars = { eob = ' ' }
+  end,
+}
+
+local tokyonight = {
+  'folke/tokyonight.nvim',
+}
+
 return {
-  {
-    -- 'folke/tokyonight.nvim',
-    -- 'sainnhe/gruvbox-material',
-    'rose-pine/neovim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_enable_bold = true
-
-      vim.cmd.colorscheme 'rose-pine'
-
-      vim.opt.fillchars = { eob = ' ' }
-    end,
-  },
+  catppuccin,
+  -- tokyonight,
+  -- rosepine,
+  -- gruvbox_material,
 }
