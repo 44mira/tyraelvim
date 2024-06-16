b68a6b5cef1e9ac912ddb8d77e8bc9c449a76eb7
44mira <legolaslada@gmail.com>
2024-06-16 19:32:21 +0800

    feat: add nordic theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -67,11 +67,32 @@ local tokyonight = {
     vim.cmd.colorscheme 'tokyonight'
   end,
 }
+
+local nordic = {
+  'AlexvZyl/nordic.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('nordic').setup {
+      bold_keywords = true,
+      bright_border = true,
+      -- transparent_bg = true,
+      -- swap_backgrounds = true,
+      leap = {
+        dim_backdrop = true,
+      },
+    }
+
+    vim.cmd.colorscheme 'nordic'
+  end,
+}
+
 -- }}}
 
 return {
   -- catppuccin,
-  tokyonight,
+  -- tokyonight,
   -- rosepine,
   -- gruvbox_material,
+  nordic,
 }
